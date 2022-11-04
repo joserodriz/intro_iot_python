9f654b599d8fc339ce511addb6fef6ed732c580d
joserodriz <109117839+joserodriz@users.noreply.github.com>
2022-11-04 00:26:10 -0300

    Add files via upload

---
+++ ejercicios_practica/practica.py
@@ -0,0 +1,6 @@
+print("Hola mundo desde un dispositivo")
+
+# Código creado para obtener la IP del dispositivo
+import platform
+plataforma = platform.system()
+print("La platafroma del dispositivo es:", plataforma)
